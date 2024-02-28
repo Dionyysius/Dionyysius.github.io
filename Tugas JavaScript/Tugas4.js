ac82d8cb3699b285eea006aea8c824a0f1e43b5f
Dionyysius <109593508+Dionyysius@users.noreply.github.com>
2024-02-28 14:36:21 +0700

    modified:   Tugas JavaScript/Tugas4.html modified:   Tugas JavaScript/Tugas4.js

--- Tugas JavaScript/Tugas4.js
+++ Tugas JavaScript/Tugas4.js
@@ -1,17 +1,24 @@
 document.getElementById('btn').addEventListener('click',function(event){
     event.preventDefault();
     var jumlah = parseInt(document.getElementById('jumlah').value);
-    console.log('nilai yang dimasukan adalah : ',jumlah)
+    console.log('nilai yang dimasukan adalah : ',jumlah);
+    createTextBoxes(jumlah);
 });
 
 function createTextBoxes(count){
     var container = document.getElementById('textbox-container');
     container.innerHTML='';
     for(var i = 0; i < count; i++){
+        var label = document.createElement('label');
+        label.textContent = 'Text'+(i+1);
+        container.appendChild(label)
+
         var input = document.createElement('input');
         input.type = 'text';
-        input.name = 'text'+(i+1);
+        input.name = 'textbox'+(i+1);
         input.placeholder = 'Text'+(i+1);
         container.appendChild(input);
+
+        container.appendChild()
     }
 };
