48cb38a6060671f9a4000697f08f40e6996b69fc
Dionyysius <109593508+Dionyysius@users.noreply.github.com>
2024-02-28 14:57:50 +0700

    modified:   Tugas JavaScript/Tugas4.js

--- Tugas JavaScript/Tugas4.js
+++ Tugas JavaScript/Tugas4.js
@@ -11,7 +11,7 @@ function createTextBoxes(count){
     for(var i = 0; i < count; i++){
         var label = document.createElement('label');
         label.textContent = 'Text'+(i+1);
-        container.appendChild(label)
+        container.appendChild(label);
 
         var input = document.createElement('input');
         input.type = 'text';
@@ -19,6 +19,42 @@ function createTextBoxes(count){
         input.placeholder = 'Text'+(i+1);
         container.appendChild(input);
 
-        container.appendChild()
+        container.appendChild(document.createElement('br'));
     }
+    var btn = document.createElement('button');
+    btn.name='buttonn';
+    btn.textContent='OK';
+    container.appendChild(btn);
+
+    
 };
+
+document.getElementById('buttonn').addEventListener('click',function(event){
+    event.preventDefault();
+
+})
+
+function createRadioButton(p){
+    var container = document.getElementById('radiobutton-container');
+    container.innerHTML='';
+    for(var i = 0; i < count; i++){
+        var label = document.createElement('label');
+        label.textContent = 'Text'+(i+1);
+        container.appendChild(label);
+
+        var radioYes = document.createElement('input');
+        radioYes.type = 'radio';
+        radioYes.name = 'radio_group'+(i+1);
+        radioYes.value = 'yes';
+        container.appendChild();
+
+        container.appendChild(document.createElement('br'));
+    }
+    var btn = document.createElement('button');
+    btn.name='buttonn';
+    btn.textContent='OK';
+    container.appendChild(btn);
+
+
+}
+
