42911574e4aa094383405e336b865793ecaa532a
Dionyysius <109593508+Dionyysius@users.noreply.github.com>
2024-02-28 15:22:51 +0700

    modified:   Tugas JavaScript/Tugas4.html modified:   Tugas JavaScript/Tugas4.js

--- Tugas JavaScript/Tugas4.js
+++ Tugas JavaScript/Tugas4.js
@@ -21,41 +21,35 @@ function createTextBoxes(count){
 
         container.appendChild(document.createElement('br'));
     }
-    var btn = document.createElement('button');
-    btn.name='buttonn';
-    btn.textContent='OK';
-    container.appendChild(btn);
+    var buttonn = document.createElement('button');
+    buttonn.name='buttonn';
+    buttonn.textContent='OK';
+    buttonn.addEventListener('click', function(){
+        createRadioButton(count);
+
+    });
+    
+    container.appendChild(buttonn);
 
 
     createRadioButton(count);
 };
 
-document.getElementById('buttonn').addEventListener('click',function(event){
-    event.preventDefault();
-
-})
-
-function createRadioButton(p){
+function createRadioButton(x){
     var container = document.getElementById('radiobutton-container');
     container.innerHTML='';
-    for(var i = 0; i < p; i++){
-        var label = document.createElement('label');
-        label.textContent = 'Text'+(i+1);
-        container.appendChild(label);
-
+    var label = document.createElement('label');
+    label.textContent = 'Pilihan';
+    container.appendChild(label);
+    for(var i = 0; i < x; i++){
         var radioYes = document.createElement('input');
         radioYes.type = 'radio';
-        radioYes.name = 'radio_group'+(i+1);
-        radioYes.value = 'yes';
+        radioYes.name = 'Text Pilihan'+(i+1);
+        radioYes.value = '';
         container.appendChild(radioYes);
-        container.appendChild(document.createTextNode('Yes'));
+        container.appendChild(document.createTextNode('Text Pilihan '));
 
-        var radioYes = document.createElement('input');
-        radioNo.type = 'radio';
-        radioNo.name = 'radio_group'+(i+1);
-        radioNo.value = 'No';
-        container.appendChild(radioNo);
-        container.appendChild(document.createTextNode('No'));
+        
 
         container.appendChild(document.createElement('br'));
     }
@@ -65,5 +59,5 @@ function createRadioButton(p){
     container.appendChild(btn);
 
 
-}
+};
 
